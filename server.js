2de7019d504fbadb2b9c27bae4607937d62a78a9
raul <zarater.raul@gmail.com>
2022-04-20 23:35:34 -0700

    finished some codeing

--- server.js
+++ server.js
@@ -2,18 +2,22 @@
 
 //Requires
 
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const {default: axios} = require('axios');
+const cors = require('cors');
 //GLOBAL VARS
 let data = require(`./weather.json`);
+const { request } = require('express');
 
 //USE
-const cors = require('cors');
 const app = express();
 app.use(cors());
 //define PORT and validate that my env is working 
-const PORT = process.env.PORT || 3002
 //if 3002, then something is wrong with the PORT
+const PORT = process.env.PORT || 3002
+const WEATHER = process.env.WEATHER_API_KEY;
+const MOVIE = process.env.MOVIE_API_KEY;
 
 
 //ROUTES
@@ -21,26 +25,58 @@ const PORT = process.env.PORT || 3002
 //app.get correlates to axios.get
 //the first param is the URL in quotes
 app.get('/', (request, response) => {
-        console.log(request.query)
         let {city} = request.query;
-        let dataToSend = data.find(city => city.city_name === area)
-        let selectedCity = new City(dataToSend);
-        response.send("Hello, from servered");
+        let dataToSend = data.find(cityData => cityData.city_name === city)
+        //let selectedCity = new City(dataToSend);
+        //response.send("Hello, from servered");
         response.send(dataToSend);
 })
 
-app.get('/sayHello', (request, response) =>
+
+app.get('/weather', async (request, response) =>
 {
-    response.send(`errorHandler: ${request.query.city_name}`);
-});
-
-app.get('/weather', (request, response) =>{
-
+    try
+    {
         let qCity = request.query.city_name;
-        let dataToSend = data.find(city => city.city_name === qCity)
-        let selectedCity = new City(dataToSend);
-    response.send(dataToSend);
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${qCity}&key=${WEATHER}`;
+        let dataToSend = await axios.get(url);
+        let arrayOfCities = [];
+        dataToSend.data.data.forEach(obj => {
+            let citiesWeather = new City(obj)
+            arrayOfCities.push(citiesWeather);
+        });
+        response.send(arrayOfCities);
+        
+    }
+    catch(e)  {response.status(500).send(error.message);}
 });
+
+app.get('/movies', async (request, response) =>
+{
+    try 
+    {
+        let qCity = request.query.city_name;
+        //https://api.themoviedb.org/3/movie/550?api_key=17f3a718b0f5d36680aae68a930ccfa4
+        let dataToSend = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE}&language=en-US&query=${qCity}&include_adult=false`);
+        //let selectedMovies = new Movies(dataToSend.data.results);
+
+        
+
+        let arrayOfMovies = [];
+        dataToSend.data.results.forEach(obj => {
+            let moviesObj = new Movies(obj)
+            arrayOfMovies.push(moviesObj);
+        });
+        response.send(arrayOfMovies);
+
+
+
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
 //CATCH ALL: error and must be at bottom of all app.gets
 app.get('*', (request, response) => {
     response.send('Error, try another request for PORT');
@@ -58,19 +94,30 @@ app.use((error, request, response, next) =>
 class City{
     constructor(cityObj)
     {
-        try
-        {
-        this.cityName = cityObj.city_name;
-        this.lon = cityObj.lon
-        this.lat = cityObj.lat
-        }
-        catch(e)
-        {
-            console.error(e.message, "you suck")
-        }
+        this.datetime = cityObj.datetime;
+        this.description = cityObj.weather.description;
     }
 }
 
+class Movies{
+    constructor(movie)
+    {
+        //this.moviesArr = [];
+        //moviesObj.forEach(movie => 
+        //{
+            //this.moviesArr.push
+            //(
+                this.title = movie.title,
+                this.overview=movie.overview,
+                this.average_votes = movie.vote_average,
+                this.total_votes= movie.vote_count,
+                this.image_url = "https://api.themoviedb.org/3/movie/550?api_key=" + movie.poster_path//.replace(/\//g,''),
+                this.popularity = movie.popularity,
+                this.release_on = movie.release_date
+            //);
+        //});
+    }
+}
 
 //Listen
 
