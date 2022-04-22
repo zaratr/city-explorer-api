7655486d6eade7a921077e4d7e08385d4eff6956
raul <zarater.raul@gmail.com>
2022-04-22 11:54:43 -0700

    commented some cache stuff

--- movies.js
+++ movies.js
@@ -1,12 +1,36 @@
 'use strict'
 
 const axios = require('axios');
+const { response } = require('express');
 module.exports = getMovies;
-
+//let cache = {};
 //const MOVIE = process.env.MOVIE_API_KEY;
 
 async function getMovies(city)
 {
+    let key = city.toLowerCase() + 'Data';
+
+    //first check if the city is in the cachealready with data
+    //and check how old
+    //if there is data, then  use that data
+    //1 month is too old
+    //let test = 1000*30;
+    /*
+    let tooOld = 1000 * 60 * 60 * 24 * 30;//1000 milli in a second 60 hours, 60 days, 24 hours, 30 days in month
+    if(cache[key] && (Date.now() - cache[key].timeStamp < tooOld))
+    {
+        console.log('it is in the cache');
+        response.status(200).send(cache[key].data)
+
+
+    }
+    //if not, then make a request and  put the data in cache
+    else{
+        console.log('did not have in cache');
+
+
+    }
+    */
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${city}&include_adult=false`;
     let dataToSend = await axios.get(url);
     let arrayOfMovies = [];
@@ -14,6 +38,15 @@ async function getMovies(city)
         let moviesObj = new Movies(obj)
         arrayOfMovies.push(moviesObj);
     });
+/*
+    //put data into the cache
+    //add a timestamp when putting into cache
+    cache[key] = {
+        data://somearray
+        timeStamp: Date.now()
+    }
+    */
+
     return arrayOfMovies;
 }
 
